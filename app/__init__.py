9576a650bfb9128d0ddc8300fa722dde158804bc
risjai <risjai@microsoft.com>
2018-03-22 11:46:33 +0530

    Return score as well

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,17 @@ app = Flask(__name__)
 pickle_in = open("extra_trees.pickle", "rb")
 classifier = pickle.load(pickle_in)
 
+#weights = {'Problem Solving': 80, 'Design': 80, 'CS Skills': 50, 'Test Enumeration': 40, 'Communication': 30}
+weights = [80,80,50,40,30]
+
+def calculateScore(data):
+    numerator = denominator = 0
+    for i in range(0, len(data) - 2):
+        if(data[i] != -1):
+            numerator += data[i]*weights[i]
+            denominator += weights[i]
+    return numerator/denominator
+
 @app.route("/", methods = ['GET', 'POST'])
 def func():
     return "Hello World!"
@@ -19,13 +30,26 @@ def hello():
         content = request.json
         print(content)
         data = np.array([content['PROBLEM_SOLVING'], content['DESIGN'], content['CS_SKILLS'],
-        content['TEST_ENUMERATION'], content['COMMUNICATION'], np.random.randint(1, 250), np.random.randint(1, 101)])
+        content['TEST_ENUMERATION'], content['COMMUNICATION'], 1000, 1000])
         print(data)
-        print(classifier.predict_proba(data.reshape(1, -1))[0][1])
-        return json.dumps({"status" : "SUCCESS" , "result": classifier.predict_proba(data.reshape(1, -1))[0][1]*100})
+        score = calculateScore(data);
+        print(score)
+        prob = classifier.predict_proba(data.reshape(1, -1))[0][1]
+        return json.dumps({"status" : "SUCCESS" , "result": prob, "score": score})
     except Exception as e:
         print("Exception is ",e)
-        return json.dumps({"status" : "SUCCESS" , "result": str(e)})
+        return json.dumps({"status" : "FAILURE" , "result": str(e)})
+
+@app.route("/api/final_score/", methods = ['POST'])
+def final_score():
+    try:
+        content = request.json
+        print(content)
+        scoreArray = [];
+        decisionArray = [];
+    except Exception as e:
+        print("Exception is ",e)
+        return json.dumps({"status" : "FAILURE" , "result": str(e)})
     
 
 if __name__ == '__main__':
